de6cc95c30bfc9a79af64324d7f95ecfdec88e60
Mira Ojamo <1149108+Siikakala@users.noreply.github.com>
2023-02-05 11:00:44 +0200

    first version

---
+++ artnet_feedback.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+
+import array, sys, dns.resolver, requests, json, math, time
+from ola.ClientWrapper import ClientWrapper
+
+wrapper = None
+mDNS=dns.resolver.Resolver()
+mDNS.nameservers=['224.0.0.251'] #mdns multicast address
+mDNS.port=5353 #mdns port
+
+MaxLedsInUniverse = 128
+Universe = 258 # Art-Net 1:0:2
+artnet_source = "10.0.0.7"
+#Nodes = ["wled-unit1", "wled-unit2", "wled-unit3", "wled-unit4", "wled-unit5", "wled-unit6", "wled-unit7", "wled-unit8", "wled-unit9", "wled-unit10"]
+Nodes = ["wled-test", "wled-Verho"]
+
+def set_bit(value, bit_index):
+    return value | (1 << bit_index)
+
+def DmxSent(status):
+  if not status.Succeeded():
+    print('Error: %s' % status.message, file=sys.stderr)
+
+  global wrapper
+  if wrapper:
+    wrapper.Stop()
+
+def updateArtnet():
+  dmxarray = array.array('B')
+  for node in Nodes:
+    statusmask = 0
+    try:
+      a = mDNS.resolve(node, 'A', 'IN', False, None, False, 0, 1) # who, what, class, tcp?, source ip, raise if no answer?, source port, lifetime
+    except:
+      a = None
+    
+    if a is not None:
+      statusmask = set_bit(statusmask,0)
+      try:
+        response = requests.get("http://"+node+"/json/info")
+      except:
+        response = None
+    
+    if response is not None:
+      statusmask = set_bit(statusmask,1)
+      data = json.load(response.json())
+      if data.live == True:
+        statusmask = set_bit(statusmask,2)
+      if data.lip == artnet_source:
+        statusmask = set_bit(statusmask,3)
+      statusmask = statusmask | (data.leds.count << 4)
+      dmxarray.append(statusmask) #chan 1
+
+      universe_amount = math.ceil(data.leds.count / MaxLedsInUniverse)
+      if universe_amount > 3:
+        universe_amount = 0
+      temperature = 0 #placeholder
+      chan2 = (universe_amount << 6) | temperature
+      dmxarray.append(chan2) #chan 2
+      
+      feedback = data.u.ArtNetFeedback
+      dmxarray.append(feedback.NetInfo.Network) #chan 3
+      dmxarray.append(feedback.NetInfo.Subnet) #chan 4
+      dmxarray.append(feedback.NetInfo.Universe) #chan 5
+      dmxarray.append(feedback.Battery.percentageLeft) #chan 6
+    else:
+      dmxarray.append(statusmask) #chan 1
+      # Padding rest of the node data with zeros
+      for i in range(2, 6):
+        dmxarray.append(0)
+
+    # Padding for future    
+    for i in range(7, 15):
+      dmxarray.append(0)
+    
+    # Per-node channel info if it's hard to parse above:
+    # * Channel 1 - statuses as bitmask
+    # ** bit 1: Does node name resolve? (mDNS - effectively testing network connectivity)
+    # ** bit 2: Is the JSON API answering?
+    # ** bit 3: Is the node receiving Art-Net?
+    # ** bit 4: ..from correct source?
+    # ** bits 5-8: amount of leds - 
+    # * Channel 2 - temperature and amount of universes bitmask
+    # ** bits 1-6: temperature with -16℃ offset to get range of 16-80
+    # ** bits 7-8: amount of universes used (unit is using too many if it's using more than 3 universes - in which case returning 0)
+    # * Channels 3-5 - Art-Net network info. Universe is starting universe.
+    # * Channel 6 - battery status
+    # * Channels 7-15 - reserved (battery cell monitoring planned among other things)
+  
+  global wrapper
+  wrapper = ClientWrapper()
+  client = wrapper.client()
+  client.SendDmx(Universe, dmxarray, DmxSent)
+  wrapper.Run()
+
+def main():
+  while True:
+    updateArtnet()
+    time.sleep(15)
+
+if __name__ == '__main__':
+  main()
